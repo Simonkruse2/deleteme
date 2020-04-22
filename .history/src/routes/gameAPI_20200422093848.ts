e50a067f97d3716f39c4c3b6826ab96d045dc27a
Simon Kristopher Kruse Bentzen <Simonkruse2@gmail.com>
2020-04-22 11:18:18 +0200

    delete me

---
+++ .history/src/routes/gameAPI_20200422093848.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import gameFacade from "../facades/gameFacade";
+const router = express.Router();
+import { ApiError } from "../errors/apiError";
+const {gameArea,players} = require("./gameData")
+
+//import * as mongo from "mongodb"
+import setup from "../config/setupDB";
+import UserFacade from "../facades/userFacadeWithDB";
+
+(async function setupDB() {
+  const client = await setup();
+  gameFacade.setDatabase(client);
+})();
+
+/*
+ Create a new polygon meant to be used on clients by React Native's MapView which
+ requres an object as the one we create below 
+ NOTE --> how we swap longitude, latitude values
+*/
+polygonForClient = {};
+polygonForClient.coordinates = gameArea.coordinates[0].map(point => {
+  return {latitude: point[1],longitude: point[0]}
+})
+
+//Returns a polygon, representing the gameArea
+app.get("/geoapi/gamearea",(req,res)=>{
+  res.json(polygonForClient);
+});
+
+router.post("/nearbyplayers", async function(req, res, next) {
+  try {
+    //Todo call your facade method
+    throw new Error("Not Yet Implemented");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Add endpoint where teams can send only POS
+router.post("/getPostIfReached", async function(req, res, next) {
+  throw new Error("Not yet implemented");
+});
+
+module.exports = router;
